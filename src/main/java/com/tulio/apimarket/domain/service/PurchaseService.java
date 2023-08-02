ac8b0c419daa6a5cd8a41f874750c7fb67ddc59d
Tulio-Rangel <tjrn20@hotmail.com>
2023-08-02 17:42:33 -0500

    Se crea el servicio de compras

---
+++ src/main/java/com/tulio/apimarket/domain/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package com.tulio.apimarket.domain.service;
+
+import com.tulio.apimarket.domain.Purchase;
+import com.tulio.apimarket.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return purchaseRepository.getByClient(clientId);
+    }
+
+    public Purchase save(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+}
